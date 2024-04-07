e7f72f146becc629c2eeae718f373d84169f4baf
Coleman Escue <robert.escue@cgi.com>
2024-04-07 13:16:02 -0400

    Implement all of the basic crud endpoints. Create podcasts service

--- src/podcasts/podcasts.controller.ts
+++ src/podcasts/podcasts.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('podcasts')
 export class PodcastsController {
-
     @Get()
     findAll() {
         return 'find all podcasts';
     }
-}
 
-// https://www.youtube.com/watch?v=sFnAHC9lLaw - 15:54 was last watched
+    @Get(':id')
+    findOne() {
+        return 'fetch podcast based on id';
+    }
+    s
+    @Put(':id')
+    update() {
+        return 'update podcast based on id';
+    }
+
+    @Post()
+    create() {
+        return 'create new podcast';
+    }
+
+    @Delete(':id')
+    delete() {
+        return 'delete podcast based on id';
+    }
+}
--- src/podcasts/podcasts.module.ts
+++ src/podcasts/podcasts.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PodcastsController } from './podcasts.controller';
+import { PodcastsService } from './podcasts.service';
 
 @Module({
-  controllers: [PodcastsController]
+  controllers: [PodcastsController],
+  providers: [PodcastsService]
 })
 export class PodcastsModule {}
